312914a9fc3e191c272781d8e6ad54035e9812c3
wtnbass <kwtnbass@gmail.com>
2017-04-13 18:16:29 +0900

    done fn add()

--- nested_projects/calc/src/lib.rs
+++ nested_projects/calc/src/lib.rs
@@ -1,6 +1,25 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn test_add() {
+        struct T {
+            input: (i32, i32),
+            output: i32,
+        }
+        let test = vec![
+            T{input: (1, 2), output: 3},
+            T{input: (-1, 2), output: 1},
+            T{input: (1, -2), output: -1},
+            T{input: (-1, -2), output: -3},
+        ];
+        for t in &test {
+            let (a, b) = t.input;
+            assert_eq!(t.output, add(a, b));
+        }
     }
 }
