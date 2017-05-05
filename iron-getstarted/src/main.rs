6450667c84834e559378e37ca71af17ba8591435
wtnbass <kwtnbass@gmail.com>
2017-05-05 09:01:10 +0900

    Add template engine

--- iron-getstarted/src/main.rs
+++ iron-getstarted/src/main.rs
@@ -1,11 +1,36 @@
 extern crate iron;
+extern crate handlebars_iron;
 
-use iron::{Request, Response, Iron, IronResult};
+use iron::{Chain, Request, Response, Iron, IronResult};
+use handlebars_iron::{DirectorySource, HandlebarsEngine, Template};
 
 fn main() {
-    Iron::new(hello_world).http("localhost:3000").unwrap();
+    let mut engine =  HandlebarsEngine::new();
+    engine.add(Box::new(DirectorySource::new("views", ".hbs")));
+    if let Err(r) = engine.reload() {
+        panic!("Error: {}", r.cause);
+    }
+
+    let mut chain = Chain::new(greeting);
+    chain.link_after(engine);
+
+    let http = "localhost:3000";
+
+    Iron::new(chain).http(http).unwrap();
+    println!("serve on {}", http);
+
 }
 
+#[allow(dead_code)]
 fn hello_world(_:&mut Request) -> IronResult<Response> {
     Ok(Response::with((iron::status::Ok, "Hello, World!")))
+}
+
+fn greeting(_: &mut Request) -> IronResult<Response> {
+    let mut data = std::collections::HashMap::new();
+    data.insert("name", "John Doe");
+
+    let tmpl = Template::new("greeting", data);
+
+    Ok(Response::with((iron::status::Ok, tmpl)))
 }
